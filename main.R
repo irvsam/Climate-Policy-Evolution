e23bed0db6f5459e3c9c0d59f04e9e9c7753795d
Sammy Irving <samantha.irving@stud.unilu.ch>
2026-03-09 15:35:26 +0100

    added a main file so no dependencies

---
+++ main.R
@@ -0,0 +1,8 @@
+# main.R
+
+# Get the data
+source("scripts/loading data/API_Connections.R")
+source("scripts/loading data/Other_data.R")
+
+# Do some initial analysis
+source("scripts/analysis/first_small_analysis.R")
