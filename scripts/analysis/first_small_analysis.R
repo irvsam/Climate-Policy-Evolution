867f86c9f2c2065f20e8baa60aa1b955879ab3b9
Sammy Irving <samantha.irving@stud.unilu.ch>
2026-03-09 15:22:40 +0100

    small initial plot before doing anything useful with the data. Forgot that I would need to restructure the data as time-series so it currently shows a drop in policies per year

--- scripts/analysis/first_small_analysis.R
+++ scripts/analysis/first_small_analysis.R
@@ -1,2 +1,21 @@
 # I want to start small just to see what I can do with the data I currently have
 
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+
+# Summarize CPDB data by year and country
+trend_data <- cpdb_master_df %>%
+  group_by(country_iso, query_year) %>%
+  summarise(policy_count = n(), .groups = 'drop')
+
+# Plot
+ggplot(trend_data, aes(x = query_year, y = policy_count, color = country_iso)) +
+  geom_line(size = 1) +
+  geom_point() +
+  theme_minimal() +
+  labs(title = "Climate Policy Adoption Trend (2015-2026)",
+       x = "Year",
+       y = "Number of Policies 'In Force'",
+       color = "Country")
