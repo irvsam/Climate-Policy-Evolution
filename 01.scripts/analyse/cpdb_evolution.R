f62191b29b3dc81ad5c93f765ace487d53785028
Sammy Irving <samantha.irving@stud.unilu.ch>
2026-03-18 10:45:54 +0100

    better idea now of who has high and who has low adaptation policies

--- 01.scripts/analyse/cpdb_evolution.R
+++ 01.scripts/analyse/cpdb_evolution.R
@@ -46,3 +46,47 @@ ggplot(adaptation_growth, aes(x = decision_date, y = cumulative_adaptation)) +
   scale_x_continuous(breaks = seq(2013, 2025, by = 1)) +
   # Adding a slight angle to the labels helps if they get crowded
   theme(axis.text.x = element_text(angle = 45, vjust = 0.5))
+
+
+# See which countries have the most adaptation policies
+
+# Calculate total unique adaptation-related policies per country (2013-2025)
+country_totals <- cpdb_clean %>%
+  filter(policy_type %in% c("adaptation", "both")) %>%
+  group_by(country_iso) %>%
+  summarise(total_policies = n_distinct(policy_id), .groups = 'drop') %>%
+  arrange(desc(total_policies)) %>%
+  # Create a rank to show the "tail" on the x-axis
+  mutate(rank = row_number())
+
+
+library(readr)
+
+# Load ND-GAIN data (only have the 2023 index)
+
+ndgain_data <- read_csv("02.data/data-raw/gain.csv") %>%
+  # Select only the most recent year if the data is longitudinal
+  # Usually ND-GAIN has columns like 'vulnerability_2022'
+  select(country_iso = ISO3, vulnerability_index = '2023')
+
+# Filter countries with > 3 policies and join
+vulnerability_report <- country_totals %>%
+  filter(total_policies > 3) %>%
+  left_join(ndgain_data, by = "country_iso") %>%
+  # Remove any rows where vulnerability index is missing
+  filter(!is.na(vulnerability_index)) %>%
+  arrange(desc(vulnerability_index)) # Sort by most vulnerable first
+
+# View the list
+print(vulnerability_report)
+
+# Compare the average nd gain amongst those with 3+ policies to those with 0-3
+average_vulnerability <- country_totals %>%
+  left_join(ndgain_data, by = "country_iso") %>%
+  mutate(policy_group = ifelse(total_policies > 3, "3+ policies", "0-3 policies")) %>%
+  group_by(policy_group) %>%
+  summarise(average_vulnerability = mean(vulnerability_index, na.rm = TRUE), .groups = 'drop')
+
+print(average_vulnerability)
+
+
