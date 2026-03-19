996c81f6868a6f21d50cf52e4e463d56e5115168
Sammy Irving <samantha.irving@stud.unilu.ch>
2026-03-19 16:26:46 +0100

    loaded in and cleaned the ndc text from the dataset

--- 01.scripts/clean/NDC_clean.R
+++ 01.scripts/clean/NDC_clean.R
@@ -5,10 +5,31 @@ library(janitor)
 
 path <- "02.data/data-raw/IGESNDC.xlsx" 
 
-# Load the data 
-ndc_raw <- read_excel(path, sheet = "NDC MASTER SHEET") %>% 
-  clean_names()
+# Read the file starting from the first actual data row (Afghanistan)
+ndc_raw_clean <- read_excel(path, 
+                            sheet = "NDC MASTER SHEET", 
+                            skip = 4, 
+                            col_names = FALSE)
 
-# Quick check of available columns
-colnames(ndc_raw)
+# Now I need to manually code column names
 
+ndc_labeled <- ndc_raw_clean %>%
+  select(
+    party = ...1, 
+    region = ...2, 
+    mitigation_text = ...10, 
+    adaptation_text = ...11
+  ) %>%
+  # Just in case there are empty rows at the bottom
+  filter(!is.na(party))
+
+install.packages("countrycode")
+library(countrycode)
+
+ndc_final <- ndc_labeled %>%
+  mutate(
+    # Convert "Afghanistan" -> "AFG", "Germany" -> "DEU"
+    iso3 = countrycode(party, origin = "country.name", destination = "iso3c")
+  ) %>%
+  # Move iso3 to the front for easier reading
+  select(iso3, everything())
